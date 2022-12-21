6278b62b70870ac56d7627c07a31307f9173c359
MohamedOuhami <mohamed.ouhami2001@gmail.com>
2022-12-21 21:12:30 +0100

    Completed Script 1 and Script 2

--- script2.py
+++ script2.py
@@ -2,18 +2,17 @@ import numpy as np
 from scipy.optimize import linprog
 
 # Vecteur des coeffcients
-C = np.array([-3,-4,-5])
+C = np.array([-24,-16])
 
 # Matrice des contraitnes
 A = np.array([
-    [0.5,1.5,2],
-    [2,1.5,0.5],
-    [0,0,0]
-]
+    [1,2],
+    [4,2]
+    ]
 )
 
 # Second membre
-B = np.array([12,15,0])
+B = np.array([150,400])
 
 # Solve the actual problem
 
